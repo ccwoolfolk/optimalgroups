66a9b693e2d32eed822a094cb34638ef6f6ffde1
Casey Woolfolk <ccwoolfolk@gmail.com>
2019-05-11 13:51:09 -0500

    Use input file instead of dummy data

--- calc.py
+++ calc.py
@@ -1,39 +1,38 @@
 import pandas as pd
 import numpy as np
-from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpMaximize, LpAffineExpression
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum
 
+MIN_IN_ALTERNATIVE = 4 # Minimum number of people (excl. 0) that can constitute a group
+NAN_VALUE = 10000 # Arbitrarily high value to force optimization away from empty choices
 rawdata = pd.read_excel('test-data.xlsx', 'Sheet1', index_col=None, na_values=['NA'])
 prob = LpProblem("Optimal 10x Grouping", LpMinimize)
 
-cost_matrix = np.array([
-    [2,3,1,10],
-    [10,1,2,3],
-])
+alternative_names = list(rawdata.columns[1:])
+persons = list(rawdata['Persons'])
+cost_matrix = rawdata.drop('Persons', 'columns').to_numpy()
 
-(n_agents, n_alternatives) = cost_matrix.shape
+(n_persons, n_alternatives) = cost_matrix.shape
 
-costs = cost_matrix.flatten()
+costs = [NAN_VALUE if np.isnan(x) else x for x in cost_matrix.flatten()]
 
-# Create binary Lp variables for every agent/alternative combination
+# Binary LpVariables for every agent/alternative combination
 choices = []
-for i in range(n_agents):
-    choices.append([])
-    for j in range(n_alternatives):
-        choices[i].append(LpVariable(f"Choice{i},{j}", cat='Binary'))
+for i in range(n_persons):
+    choices.append([LpVariable(f"Choice:{persons[i]},{alternative_names[j]}", cat='Binary') for j in range(n_alternatives)])
 
-has_membership = [LpVariable(f"Choice{j}HasMembership", cat='Binary') for j in range(n_alternatives)]
+# Binary LpVariables tracking if a group has any members
+has_membership = [LpVariable(f"{alternative_names[j]}:HasMembership", cat='Binary') for j in range(n_alternatives)]
 
 # Add constraints
-for i in range(n_agents):
+for i in range(n_persons):
     prob += sum(choices[i]) == 1 # Only one result per agent
 
-min_in_alternative = 1 # i.e., min ppl in a group if > 0
 for j in range(n_alternatives):
     prob += has_membership[j] <= 1
-    for i in range(n_agents):
+    for i in range(n_persons):
         prob += has_membership[j] >= choices[i][j]
     # If a group has any members, enforce a minimum number
-    prob += sum([choices[i][j] for i in range(n_agents)]) >= min_in_alternative * has_membership[j]
+    prob += sum([choices[i][j] for i in range(n_persons)]) >= MIN_IN_ALTERNATIVE * has_membership[j]
 
 choices_full = np.array(choices).flatten()
 prob += lpSum([choices_full[i] * costs[i] for i in range(len(choices_full))])
