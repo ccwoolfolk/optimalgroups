cbb344ca743afd1a8656a56cf65ed11df893de83
Casey Woolfolk <ccwoolfolk@gmail.com>
2019-05-21 20:57:41 -0500

    Implement package file structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='OptimalGroups',
+    version='0.1dev',
+    packages=['optimalgroups',],
+    license='Blue Oak Model License Version 1.0.0',
+    long_description=open('README.txt').read(),
+    scripts=['bin/og.py'],
+    install_requires=[
+        'argparse',
+        'pandas',
+        'numpy',
+        'pulp',
+    ],
+)
