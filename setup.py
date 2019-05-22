b409f6589fc3370280848d347c20174a280b999b
Casey Woolfolk <ccwoolfolk@gmail.com>
2019-05-21 21:02:37 -0500

    Update README file extension

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1dev',
     packages=['optimalgroups',],
     license='Blue Oak Model License Version 1.0.0',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     scripts=['bin/og.py'],
     install_requires=[
         'argparse',
