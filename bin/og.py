6ea2000a1834673bae099214c70d32ca56b1755a
Casey Woolfolk <ccwoolfolk@gmail.com>
2019-05-22 20:18:16 -0500

    Lint file

--- bin/og.py
+++ bin/og.py
@@ -1,1 +1,2 @@
 from optimalgroups import main
+main.optimize()
--- optimalgroups/main.py
+++ optimalgroups/main.py
@@ -1,4 +1,5 @@
-import argparse
+"""CLI for optimal subgroups based on individual preferences"""
+from argparse import ArgumentParser
 import pandas as pd
 import numpy as np
 from pulp import LpProblem, LpMinimize, LpVariable, lpSum
@@ -6,65 +7,95 @@ from pulp import LpProblem, LpMinimize, LpVariable, lpSum
 NAN_VALUE = 10000 # Arbitrarily high value to force optimization away from empty choices
 
 
-parser = argparse.ArgumentParser(description='Sort a group into optimal preference-based subgroups.')
-parser.add_argument('min', type=int, help='Min group size if group has > 0')
-parser.add_argument('file_path', help='Path to input file with "Persons" in first column')
+def optimize():
+    """Run the optimizer"""
+    args = get_args()
+    # Assemble data
+    rawdata = pd.read_excel(args.file_path, 'Sheet1', index_col=None, na_values=['NA'])
+    prob = LpProblem("Optimal 10x Grouping", LpMinimize)
 
-args = parser.parse_args()
+    alternatives = list(rawdata.columns[1:])
+    persons = list(rawdata['Persons'])
+    cost_matrix = rawdata.drop('Persons', 'columns').to_numpy()
+    (n_persons, n_alternatives) = cost_matrix.shape
 
-
-# Assemble data
-rawdata = pd.read_excel(args.file_path, 'Sheet1', index_col=None, na_values=['NA'])
-prob = LpProblem("Optimal 10x Grouping", LpMinimize)
-
-alternative_names = list(rawdata.columns[1:])
-persons = list(rawdata['Persons'])
-cost_matrix = rawdata.drop('Persons', 'columns').to_numpy()
-(n_persons, n_alternatives) = cost_matrix.shape
-
-costs = [NAN_VALUE if np.isnan(x) else x for x in cost_matrix.flatten()]
+    costs = [NAN_VALUE if np.isnan(x) else x for x in cost_matrix.flatten()]
 
 
-# Create binary LpVariables for every agent/alternative combination
-choices = []
-for i in range(n_persons):
-    choices.append([LpVariable(f"Choice:{persons[i]},{alternative_names[j]}", cat='Binary') for j in range(n_alternatives)])
-
-
-# Create binary LpVariables tracking if a group has any members
-has_membership = [LpVariable(f"{alternative_names[j]}:HasMembership", cat='Binary') for j in range(n_alternatives)]
-
-
-# Add constraints
-for i in range(n_persons):
-    prob += sum(choices[i]) == 1 # Only one result per agent
-
-for j in range(n_alternatives):
-    prob += has_membership[j] <= 1
+    # Create binary LpVariables for every agent/alternative combination
+    choices = []
+    make_name = make_var_name_factory(persons, alternatives)
     for i in range(n_persons):
-        prob += has_membership[j] >= choices[i][j]
-    # If a group has any members, enforce a minimum number
-    prob += sum([choices[i][j] for i in range(n_persons)]) >= args.min * has_membership[j]
+        choices.append([LpVariable(make_name(i, j), cat='Binary') for j in range(n_alternatives)])
 
 
-# Define and calculate the optimization
-choices_full = np.array(choices).flatten()
-prob += lpSum([choices_full[i] * costs[i] for i in range(len(choices_full))])
-prob.solve()
-optimized_value = prob.objective.value()
-perfect_value = len(persons)
-worst_value = sum([np.nanmax(x) for x in cost_matrix])
-optimization_score = 100 - round(100 * (optimized_value - perfect_value) / (worst_value - perfect_value), 0)
+    # Create binary LpVariables tracking if a group has any members
+    has_membership = [LpVariable(f"{alternatives[j]}", cat='Binary') for j in range(n_alternatives)]
 
 
-# Display results
-print(f"Optimization score (0-100): {optimization_score}")
-print(f"Achieved: {optimized_value}")
-print(f"Perfect: {perfect_value}")
-print(f"Worst: {worst_value}")
+    # Add constraints
+    for i in range(n_persons):
+        prob += sum(choices[i]) == 1 # Only one result per agent
 
-for j in range(n_alternatives):
-    selected_persons = [persons[i] for i in range(n_persons) if choices[i][j].varValue == 1.0]
-    if len(selected_persons) > 0:
-        print(f"\n{alternative_names[j]}:")
-        [print(f"  {person}") for person in selected_persons]
+    for j in range(n_alternatives):
+        prob += has_membership[j] <= 1
+        for i in range(n_persons):
+            prob += has_membership[j] >= choices[i][j]
+        # If a group has any members, enforce a minimum number
+        prob += sum([choices[i][j] for i in range(n_persons)]) >= args.min * has_membership[j]
+
+
+    # Define and calculate the optimization
+    choices_full = np.array(choices).flatten()
+    prob += lpSum([choices_full[i] * costs[i] for i in range(len(choices_full))])
+    prob.solve()
+
+    display_results(
+        optimized_value=prob.objective.value(),
+        persons=persons,
+        alternatives=alternatives,
+        choices=choices,
+        cost_matrix=cost_matrix,
+    )
+
+
+def get_args():
+    """Parse CLI arguments"""
+    parser = ArgumentParser(description='Sort a group into optimal preference-based subgroups.')
+    parser.add_argument('min', type=int, help='Min group size if group has > 0')
+    parser.add_argument('file_path', help='Path to input file with "Persons" in first column')
+
+    return parser.parse_args()
+
+
+def make_var_name_factory(person_list, alternative_list):
+    """Create an LpVariable name factory that accepts indexes"""
+    def name_factory(i, j):
+        person = person_list[i]
+        alternative = alternative_list[j]
+        return f"Choice:{person},{alternative}"
+    return name_factory
+
+
+def display_results(optimized_value, persons, alternatives, choices, cost_matrix):
+    """Print results to the console"""
+    (n_persons, n_alternatives) = cost_matrix.shape
+    perfect_value = n_persons
+    worst_value = sum([np.nanmax(x) for x in cost_matrix])
+    score = 100 - round(100 * (optimized_value - perfect_value) / (worst_value - perfect_value), 0)
+
+    print(f"Optimization score (0-100): {score}")
+    print(f"Achieved: {optimized_value}")
+    print(f"Perfect: {perfect_value}")
+    print(f"Worst: {worst_value}")
+
+    for j in range(n_alternatives):
+        selected = [persons[i] for i in range(n_persons) if choices[i][j].varValue == 1.0]
+        if selected:
+            print(f"\n{alternatives[j]}:")
+            for person in selected:
+                print(f"  {person}")
+
+
+if __name__ == "__main__":
+    optimize()
